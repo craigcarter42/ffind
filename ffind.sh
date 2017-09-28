252372f01730cbeb76b78ebcca87e52e18042966
craigcarter42 <32251992+craigcarter42@users.noreply.github.com>
2017-09-28 15:42:00 -0700

    Update ffind.sh

--- ffind.sh
+++ ffind.sh
@@ -3,38 +3,54 @@ EXT=".txt"
 NAME="results_"
 DT_STAMP=`date`
 T_STAMP=`date +"%T"`
-FILE_NAME=$NAME$T_STAMP$EXT
-VAR_NUM=0
-VAR_PATH=""
+file_name=$NAME$T_STAMP$EXT
+usr_input=0
+file_path=''
 
 perfrom_search() {
 	if [ $SEARCH_WORD ]; then
-		echo ':: searching...'
-		echo ":: date and time: $DT_STAMP" >> /Applications/ffind/results/$FILE_NAME
-		echo ":: keyword: $SEARCH_WORD\n" >> /Applications/ffind/results/$FILE_NAME
+		working_dir=`pwd`
+		results_path="/results/$file_name"
+		final_path=$working_dir$results_path
+		echo "$(tput setaf 2)::$(tput sgr0)" "searching..."
+		echo ":: date and time: $DT_STAMP" >> $final_path
+		echo ":: directory: $file_path" >> $final_path
+		echo ":: keyword: $SEARCH_WORD\n" >> $final_path
 		cd ~
-		cd $VAR_PATH # < ---- starting search directory
-		find . -name $SEARCH_WORD >> /Applications/ffind/results/$FILE_NAME
-		echo ':: end\n'
-		echo ':: displaying last search result: '$FILE_NAME
-		cat /Applications/ffind/results/$FILE_NAME
+		cd $file_path # < ---- starting search directory
+		find . -name $SEARCH_WORD >> $final_path
+		echo "$(tput setaf 2)::$(tput sgr0) end\n"
+		echo "$(tput setaf 2)::$(tput sgr0) displaying last search result: $file_name"
+		cat $final_path
 	else
-		echo ':: please enter a search word'
+		echo "$(tput setaf 1):: $(tput sgr0) invaild input"
+		start
 	fi
 }
 
-echo ':: ffind: started\n:: (1) basic search (2) advanced search'
-read -p '>> ' VAR_NUM
-if [ $VAR_NUM == '1' ]; then
-	echo ':: enter a search word:'
-	read -p '>> ' SEARCH_WORD
-	perfrom_search
-elif [ $VAR_NUM == '2' ]; then
-	echo ':: enter a search word:'
-	read -p '>> ' SEARCH_WORD
-	echo ':: enter starting directory'
-	read -p '>> ' VAR_PATH
-	perfrom_search
-else
-	echo ':: invaild input'
-fi
+start() {
+	echo "$(tput setaf 2)::$(tput sgr0) (1) basic search (2) advanced search (3) help (e) exit"
+	read -p '>> ' usr_input
+	if [ "$usr_input" == "1" ]; then
+		echo "$(tput setaf 2)::$(tput sgr0) enter a search word:"
+		read -p '>> ' SEARCH_WORD
+		perfrom_search
+	elif [ "$usr_input" == "2" ]; then
+		echo "$(tput setaf 2)::$(tput sgr0) enter a search word:"
+		read -p '>> ' SEARCH_WORD
+		echo "$(tput setaf 2)::$(tput sgr0) enter starting directory"
+		read -p '>> ' file_path
+		perfrom_search
+	elif [ "$usr_input" == "3" ]; then
+		cat help.txt; echo
+		start
+	elif [ "$usr_input" == "e" ]; then
+		echo "$(tput setaf 1)::$(tput sgr0) ffind exited"
+		exit
+	else
+		echo "$(tput setaf 1):: $(tput sgr0) invaild input"
+		start
+	fi
+}
+echo "$(tput setaf 2):: $(tput sgr0)" "ffind: started"
+start
