bbde4a2cc5de682d4995a102d85a22244d259d62
Craig Carter <32251992+craigcarter42@users.noreply.github.com>
2017-09-29 16:50:34 -0700

    Update ffind.sh

--- ffind.sh
+++ ffind.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
-EXT=".txt"
-NAME="results_"
+
+# get current date:
 DT_STAMP=`date`
-T_STAMP=`date +"%T"`
-file_name=$NAME$T_STAMP$EXT
+
+# create file name:
+ext=".txt"
+name="results_"
+time_stamp=`date +"%T"`
+file_name=$name$time_stamp$ext
+
+# get current working directory and make file path:
+working_dir=`pwd`
+results_path="/results/$file_name"
+final_path=$working_dir$results_path
+
+# general:
 usr_input=0
 file_path=''
+caps='"'
+SEARCH_WORD=''
+ffind='find . -name '
 
 perform_search() {
-	if [ $SEARCH_WORD ]; then
-		working_dir=`pwd`
-		results_path="/results/$file_name"
-		final_path=$working_dir$results_path
+	if [ "$SEARCH_WORD" != "" ]; then
 		echo "$(tput setaf 2)::$(tput sgr0) searching..."
+		if [ $file_path ]; then
+			eval cd $file_path # < ---- starting search directory
+		else
+			eval cd ~
+			file_path="~"
+		fi
 		echo ":: date and time: $DT_STAMP" >> $final_path
 		echo ":: directory: $file_path" >> $final_path
 		echo ":: keyword: $SEARCH_WORD\n" >> $final_path
-		cd ~
-		cd $file_path # < ---- starting search directory
-		find . -name $SEARCH_WORD >> $final_path
+		eval $final_search_word >> $final_path
 		echo "$(tput setaf 2)::$(tput sgr0) end\n"
 		echo "$(tput setaf 2)::$(tput sgr0) displaying last search result: $file_name"
 		cat $final_path
@@ -34,12 +49,14 @@ start() {
 	if [ "$usr_input" == "1" ]; then
 		echo "$(tput setaf 2)::$(tput sgr0) enter a search word:"
 		read -p '>> ' SEARCH_WORD
+		final_search_word=$ffind$caps$SEARCH_WORD$caps
 		perform_search
 	elif [ "$usr_input" == "2" ]; then
 		echo "$(tput setaf 2)::$(tput sgr0) enter a search word:"
 		read -p '>> ' SEARCH_WORD
 		echo "$(tput setaf 2)::$(tput sgr0) enter starting directory"
 		read -p '>> ' file_path
+		final_search_word=$ffind$caps$SEARCH_WORD$caps
 		perform_search
 	elif [ "$usr_input" == "3" ]; then
 		cat help.txt; echo
@@ -52,5 +69,28 @@ start() {
 		start
 	fi
 }
-echo "$(tput setaf 2)::$(tput sgr0) ffind: started"
-start
+
+if [ "$1" = "-f" ] && [ "$3" = "-p" ]; then
+	if [ $2 ] && [ $4 ]; then
+		echo "$(tput setaf 2)::$(tput sgr0) advanced search"
+		SEARCH_WORD=$2
+		final_search_word=$ffind$caps$SEARCH_WORD$caps
+		file_path=$4
+		perform_search
+	else
+		echo "$(tput setaf 1)::$(tput sgr0) invaild input"
+	fi
+elif [ "$1" = "-f" ]; then
+	if [ $2 ]; then
+		echo "$(tput setaf 2)::$(tput sgr0) basic search"
+		SEARCH_WORD=$2
+		final_search_word=$ffind$caps$SEARCH_WORD$caps
+		perform_search
+	else
+		echo "$(tput setaf 1)::$(tput sgr0) invaild input"
+	fi
+else
+	echo "$(tput setaf 2)::$(tput sgr0) ffind: started"
+	start
+fi
+
